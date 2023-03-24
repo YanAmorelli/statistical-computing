6247678981174407d86e69f24dc629c1697fd040
Yan Liao Amorelli <73948655+YanAmorelli@users.noreply.github.com>
2023-03-24 16:06:43 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -11,15 +11,38 @@ m_op[0, 0] = math.sqrt(m_dada[0, 0])
 for i in range(len(m_op) -1):
     m_op[0, i + 1] = m_dada[0, i + 1]/ m_op[0, 0]
 
-# Se𝑛≠2.
+# Se 𝑛≠2.
 # Para j > 1 até len(m_op -2) 
+# Calculando linhas do meio
 if  len(m_op) != 2:
-    j = 2
-    for j in range(len(m_op -2)):
-        if j != 0:
-            m_op[j, j] = m_dada[j, j] - 
-            print(j)
-        
+    for j in range(len(m_op)):
+        if (j > 0) and (j < len(m_op) -1):
 
-print(m_op)
+            ## Calculando somatório para determinante
+            summatory_todet = 0
+            for k in range(j): 
+                summatory_todet = summatory_todet + m_op[k, j]
+                
+            m_op[j, j] = math.sqrt(m_dada[j, j] - math.pow(summatory_todet, 2))
+            summatory_todet = 0
+            
+            for l in range(j):                
+                print("j", j)
+                print("l", l + 1)
+                    
+                ## Calculando somatório para os lados
+                summatory_tosides = 0                
+                for m in range(j):
+                    print("m", m)
+                    summatory_tosides = m_op[m, j]*m_op[m, l + 2]
+                    
+            m_op[j, l + 2] = (m_dada[j, l + 2] - summatory_tosides)/m_op[j, j]
 
+# Calculando último canto da matriz[n, n]
+## Calculando somatório para n, n
+summatory_tonn = 0
+for i in range(len(m_op) - 1):
+    summatory_tonn = summatory_tonn + math.pow(m_op[i, len(m_op) - 1], 2)
+    
+m_op[len(m_op) - 1, len(m_op) - 1] = math.sqrt(m_dada[len(m_dada) - 1, len(m_dada) - 1] - summatory_tonn)
+print("m_op", m_op)
